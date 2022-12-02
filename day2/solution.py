0ce34be5a596e43dd4cd47d5389c7bc4324ff0f0
anna-parfenova <anna.parfenova@deliveryhero.com>
2022-12-02 07:27:52 +0100

    DAY2_solution

--- day2/solution.py
+++ day2/solution.py
@@ -1,29 +1,49 @@
+RULES = {"A": (1, "C", "B"), "B": (2, "A", "C"), "C": (3, "B", "A")}
+
 def parse_data(input_data):
     input_lines = input_data.split("\n")
     result = []
-    subres = []
     for il in input_lines:
-        if il != "":
-            subres.append(int(il))
+        subres = il.split(" ")
+        result.append(subres)
+    return result
+
+
+def part_one(data):
+    rules = RULES
+    xyz = {"X": "A", "Y": "B", "Z": "C"}
+    total = 0
+    for elf, you in data:
+        you = xyz[you]
+        if you == elf:
+            score = 3 + rules[you][0]
+        elif elf == rules[you][1]:
+            score = 6 + rules[you][0]
         else:
-            result.append(subres)
-            subres = []
-    result.append(subres)
-    return result
+            score = rules[you][0]
+        total += score
+    return total
 
-
-def get_n_max(kcals, n):
-    sums = []
-    for elf in kcals:
-        sums.append(sum(elf))
-    sums.sort(reverse=True)
-    result = sum(sums[:n])
-    return result
+def part_two(data):
+    rules = {"A": (1, "C", "B"), "B": (2, "A", "C"), "C": (3, "B", "A")}
+    total = 0
+    for elf, you in data:
+        if you == "X":
+            you = rules[elf][1]
+            score = rules[you][0]
+        elif you == "Y":
+            you = elf
+            score = 3 + rules[you][0]
+        else:
+            you = rules[elf][2]
+            score = 6 + rules[you][0]
+        total += score
+    return total
 
 
 if __name__ == "__main__":
-    with open("input.txt", "r") as file:
+    with open("test.txt", "r") as file:
         data = file.read()
     input = parse_data(data)
-    print(f"The answer for the 1st task is: {get_n_max(input, 1)}")
-    print(f"The answer for the 2nd task is: {get_n_max(input, 3)}")
+    print(f"The answer for the 1st task is: {part_one(input)}")
+    print(f"The answer for the 2nd task is: {part_two(input)}")
